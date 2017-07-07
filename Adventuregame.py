252b2b34c5292dbaca26d02c8210e2963b320015
Jenniferperez1004 <jjjjfp1004@gmail.com>
2017-07-07 15:52:48 -0400

    Create Adventuregame.py

---
+++ Adventuregame.py
@@ -0,0 +1,24 @@
+start = '''
+You wake up and find out your dog is gone. You try to  get his treats and call him but you can't find him, you panick.
+You think, where could he go? Only two places that he'll go too.. '''
+
+print(start)
+
+print("Type 'left' to go to the neighbor down the street or 'right'to go to the dog park .")
+user_input = input()
+if user_input == "left":
+    print("Type left to bang on the neighbors door and ask if she has your dog! or right to sneak into her backyard and look for your dog.")
+elif user_input == "right":
+    print ("You chose to go right and go to the dog park.")
+if user_input == "left":
+    print ("you decided to go left and bang on the neighbors door and ask if she has your dog!She replies no, but she has seen him earlier. You ask where and she couldn't give you a clear response. Her oatmel was altering her words from becoming understandable to hear. You now decide: . ")
+    print ("Type 'left'and force her to let you in or 'right'to go to the dog park.")
+elif user_input == "right"
+    print ("You go to the dog park but you realize your ex and your boss are there. Your ex has been waiting for you at the dog park for 2 weeks now and you lied to your boss that you were sick to sleep in today: what will you decide to do: .")
+if user_input == "left":
+    print ("you decide to force her to go in and she calls the cops on you for trespassing...game over.")
+    print ("Type 'left'if you would rather pass your ex to look for your dog in the dog park or 'right'to walk past your boss whom you lied too.")
+if user_input == "left":
+print ("you decided to go right and your boss questions you on what you are doing at the dog park, you start to panick and stutter when you are trying to come up with a good lie off the top of your head.")
+    print ("Type 'left' if you would want to call the police on your ex or 'right' to start coughing dramatically to make your story true.")
+
